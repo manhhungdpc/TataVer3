141c3f5348ce9c43879e327b16fa7d735543393a
manhhungdev <manhhungdpc@gmail.com>
2016-04-24 14:07:14 +0700

    TataEnglishVer2
    tata english

---
+++ views/layouts/main.php
@@ -0,0 +1,121 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+  <meta charset="<?= Yii::$app->charset ?>">
+  <link href='https://fonts.googleapis.com/css?family=Passion+One:400,800' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?= Html::csrfMetaTags() ?>
+  <title><?= Html::encode($this->title) ?></title>
+  <?php $this->head() ?>
+</head>
+<body>
+  <?php $this->beginBody() ?>
+  <?php 
+  $logo=Html::img(Yii::$app->request->baseUrl.'/image/logo.png',array('width'=>'269px','height'=>'50%','title'=>'home page','id' => 'logo'));
+  $btnDangKy=Html::a('Đăng ký học thử miễn phí','https://docs.google.com/forms/d/1uP3xRPPnzhGgF7ikqCvxzC9UDSA5qOAU4MiNe2Kx9y4/viewform',['class' => 'btn btn-danger']);
+  $menuItems = [
+  ['label' => 'Home', 'url' => ['/site/index']],
+  ['label' => 'study', 'url' => ['/site/study']]
+  /*'items'=>[
+  ['label'=>'homework','url'=>['#']],
+  ['label'=>'listening','url'=>['#']],
+  ['label'=>'writing','url'=>['#']],
+  ['label'=>'grammar','url'=>['#']],
+  ['label'=>'typing','url'=>['#']],
+  ],
+  'options'=>['class'=>'nav NavBar-nav'],*/
+  ];
+  if (Yii::$app->user->isGuest) {
+    #$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+    $menuItems[] = ['label' => 'Log in', 'url' => ['/site/login']];
+  } else {
+    $menuItems[] = '<li>'
+    . Html::beginForm(['/site/logout'], 'post')
+    . Html::submitButton(
+      'Logout (' . Yii::$app->user->identity->username . ')',
+      ['class' => 'btn btn-link']
+      )
+    . Html::endForm()
+    . '</li>';
+    $menuItems[] = ['label' => 'feedback', 'url' => ['#']];
+  }
+  ?>
+  <div class="container">   
+    <header class="clearfix">
+      <div class="row">
+        <div class="col-sm-12 col-md-2 col-lg-2">
+          <?=$logo;?> 
+        </div>
+        <?php if(Yii::$app->user->isGuest){
+          echo '<div class="col-xs-12 col-sm-12 col-md-2 col-md-push-6">'.$btnDangKy.'</div>';
+        } ?>
+        <div class="col-xs-12 col-sm-12 col-md-4 col-md-rush-5">
+          <nav>
+            <?=Nav::widget([
+              'options' => ['class' => 'menu'],
+              'items' => $menuItems,
+              ]);
+              ?>                          
+            </nav>
+          </div>
+          <div class="clearfix"></div>
+        </div>                    
+      </header> 
+      <div class="container">
+        <?= Breadcrumbs::widget([
+          'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+          ]) ?>
+          <?= Alert::widget() ?>
+          <?= $content ?>
+        </div>
+        <?php $footer ='<footer class="footer">
+          <div class="container">
+              <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                  <address>
+                    <strong>Tata English</strong><br>
+                    Số 9, ngõ 1, đường Cầu Vồng, Đức Thắng, Bắc Từ Liêm, Hà Nội.<br>
+                    <abbr title="Phone">P:+84 0462 544 288<br>
+                      +84 0981 222 085<br>
+                    </abbr>
+                    <a href="https://www.facebook.com/tata.edu.vn/?fref=ts">Page Facebook</a>
+                  </abbr> 
+                </address>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d29781.04442587521!2d105.781308!3d21.087413!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xea04d15c4e0ccb03!2sTATA+Learning+English+for+Future!5e0!3m2!1sen!2sus!4v1461201342675" width="100%" height="80%" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                <?php
+
+                ?>
+              </div>
+            </div>  
+          <div class="clearfix"></div>
+        </div>
+      </footer>
+    </div>' ?>
+    <?php if(Yii::$app->user->isGuest){
+          echo $footer;
+        } ?>
+    <?php $this->endBody() ?>
+  </body>
+  </html>
+  <?php $this->endPage() ?>
---
+++ views/site/index.php
@@ -0,0 +1,167 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\bootstrap\ActiveForm;
+use frontend\assets\AppAsset;
+use yii\helpers\BaseUrl;
+use \yii\base\Model;
+
+AppAsset::register($this);
+?>
+<?php
+$this->title = 'Tata English';
+?>
+<?php
+$images = [
+Html::img(Yii::$app->request->baseUrl.'/image/logo.png',array('width'=>'269px','height'=>'50%','title'=>'home page','id'=>'class'))
+,
+Html::img(Yii::$app->request->baseUrl.'/image/page1_icon1.png',array('class'=>'icon-s'))
+,
+Html::img(Yii::$app->request->baseUrl.'/image/page1_icon2.png',array('class'=>'icon-s'))
+,
+Html::img(Yii::$app->request->baseUrl.'/image/page1_icon3.png',array('class'=>'icon-s'))
+,
+Html::img(Yii::$app->request->baseUrl.'/image/page1_pic4.jpg',array('id'=>'footer-image'))
+,
+Html::img(Yii::$app->request->baseUrl.'/image/gift.png',array('class'=>'gift','width'=>'50px','height'=>"50px"))
+];
+?>
+<div class="header-bot">
+  <div class="sub-bot-top">
+    <div class="row">
+      <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+        <p class="big-size lead">PREPARING STUDENTS FOR SUCCESSFUL FUTURE!</p>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1"><p class="small-size lead">Life isn't about finding yourself , life is about creating yourself</p>
+        </div>
+      </div>
+    </div>
+    <div class="clearfix">
+    </div>
+  </div>
+</div>
+<div id="contents">
+  <div class="container">
+    <div class="sub-main-top">
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="thump">
+          <div class="bot-image">
+            <?=$images[1]; ?>
+          </div>
+          <p class="medium-size">Lộ trình học tập</p></br>
+          <p class="small-size">
+            <span >Level 1: Phát âm, nghe nói phản xạ</span><br>
+            <span >Level 2: Nghe nói đọc viết cơ bản</span><br>
+            <span >Level 3: Nghe nói đọc viết nâng cao</span><br>
+            <span >Level 4: Cam kết đầu ra TOEIC 700+ </span><br>
+          </p>
+          <button class="btn btn-primary">Read more</button>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">  
+        <div class="thump">
+          <div class="bot-image ">
+            <?=$images[2]; ?>
+          </div>
+          <p class="medium-size">Trang web tự học</p></br>
+          <p class="small-size">
+            <span>Học bằng âm thanh, hình ảnh sinh động</span></br>
+            <span>Từ vựng chỉ cần học 1 lần là thuộc làu</span></br>
+            <span>Học ngữ pháp ứng dụng vào 4 kĩ năng</span></br>
+            <span>Luyện nghe chưa bao giờ lại dễ đến thế</span></br>
+          </p>
+          <button class="btn btn-primary">Read more</button>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="thump">
+          <div class="bot-image">
+            <?=$images[3];  ?>
+          </div>
+          <p class="medium-size">Cảm nhận học sinh</p></br>
+          <p class="small-size">
+            <span>"Đại học cũng bốn năm rồi</span></br>
+            <span>Toàn là nằm ngủ không thì chơi game</span></br>
+            <span>Ngoài ra chẳng biết gì thêm</span></br>
+            <span>Thời gian cứ thế êm đềm trôi qua". </span></br>
+          </p>
+          <button class="btn btn-primary" >Read more</button>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+    <div class="sub-main-mid">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+        <div class="sologan">
+          <p class="medium-size">Video học sinh:</p>
+          <p class="small-size">Cuối cùng khi tốt nghiệp tại TATA thì các học viên không chỉ đạt được khả năng Nghe, Nói, Đọc, Viết thành thạo để áp dụng vào cuộc sống và công việc sau này mà còn đạt được ít nhất 700 điểm Toeic.</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+          <div class="thump">
+            <iframe width="100%" height="235"src="https://www.youtube.com/embed/CU8fg8j2Jmk"frameborder="0"allowfullscreen></iframe>
+            <p class="medium-size">Tốt nghiệp level 1</p>
+            <p class="small-size"><span>Chỉ sau 20 tuần là toàn bộ các học viên đã có thể phát âm chuẩn tất cả các từ theo từ điển Oxford và có thể nói chuyện một cách thoải mái, tự nhiên về các vấn đề quen thuộc trong cuộc sống.</span></p>
+            <button class="btn btn-primary">Read more</button>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+          <div class="thump"> 
+            <iframe width="100%"height="235"src="https://www.youtube.com/embed/8QmQOUQWVq0"frameborder="0"allowfullscreen></iframe>
+            <p class="medium-size">Tốt nghiệp level 2</p>
+            <p class="small-size">Và sau 20 tuần tiếp theo các học viên không chỉ nắm vững tất cả các cấu trúc ngữ pháp áp dụng vào Nghe, Nói, Đọc, Viết mà còn có thể nói chuyện trơn tru, tự nhiên với những người nước ngoài.</p>  
+            <button class="btn btn-primary">Read more</button></div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="thump">
+              <iframe width="100%"height="235"src="https://www.youtube.com/embed/HNeanpYXr3A"frameborder="0"allowfullscreen></iframe>
+              <p class="medium-size">Cam kết đầu ra</p>
+              <p class="small-size"><span>Hầu hết học sinh đến với TATA đều bắt đầu từ con số không, phát âm thì sai đến 99%.
+                Thế nhưng ... </span>
+              </p>
+              <button class="btn btn-primary">Read more</button>
+            </div>  
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+      <div class="sub-main-bot">
+        <div class="row">
+          <div class="col-xs-12 col-ms-12 col-md-4 col-lg-4">
+            <div class="thump">
+              <div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+                <?=$images[5] ?>
+              </div>
+              <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                <p class="small-size"><span>Mời bạn tham gia 2 tuần học thử miễn phí tại TATA để nhận ra rằng mình hoàn toàn có thể tự học được cả 4 kỹ năng Nghe, Nói, Đọc, Viết trong Tiếng Anh</span></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-8">
+            <?=$images[4]; ?>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12 ">
+            <button class="btn btn-primary">Đăng ký học thử miễn phí</button>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="main-bot">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12m">
+              <p class="big-size">DOWNLOAD TỪ ĐIỂN OXFORD</p>
+            </div>
+            <div class="col-xs-12  col-sm-12 col-md-12 col-lg-12"><p class="medium-size">
+              <p>Từ điển Oxford là từ điển tốt nhất thế giới để luyện phát âm, tra cứu các hiện tượng ngữ pháp và cách sử dụng các từ vựng..</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <button class="btn btn-danger">Download</button>
+            </div>
+            <div class="clear-fix"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
